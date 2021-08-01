30e1bf6830a314e5285baf8a9799be08c14d9b9b
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2021-08-01 02:31:13 +0200

    created new project and added icons

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Zdenko Čepan on 01/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocados/Avocados/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocados
+//
+//  Created by Zdenko Čepan on 01/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
